1e645d648fac8c0fc3be885be889efb825e39f32
Your Name <you@example.com>
2022-08-22 17:39:07 +0900

    add readme

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Simple use
 from shrt_id import ShortId
 id = ShortId().id()
 print(id)
-# wiwdhP8Q (default length is 8)
+# ev60bb7Q (default length is 8)
 ```
 
 Custom length
@@ -28,6 +28,27 @@ from shrt_id import ShortId
 id = ShortId().id(10)
 print(id)
 # wiwdhx8V7x
+```
+
+Get Id and shard at same time
+
+```python
+from shrt_id import ShortId
+id, shard = ShortId().id_with_shard(10)
+print(id, shard)
+# ev60fM2z89
+```
+
+Decode id for datetime and shard
+
+```python
+from shrt_id import ShortId
+id, shard = ShortId().id_with_shard()
+print(id, shard)
+# ev60rHdz 31
+decoded_id = ShortId().decode(id)
+print(decoded_id)
+# {'shard_seq': 31, 'dt': datetime.datetime(2022, 8, 22, 17, 37, 18, 300000), 'random_string': 'dz', 'random_seq': 211}
 ```
 
 ### Thanks
--- src/shrt_id/__init__.py
+++ src/shrt_id/__init__.py
@@ -1,38 +1,74 @@
 from datetime import datetime
 import time
-import string
 import random
-
 DEFALT_LENGTH = 8
-alpha = string.ascii_letters + string.digits
-alpha_len = len(alpha)
 
 
 class ShortId:
-    def __init__(self, shard: int = 60, start_date: datetime = datetime(2020, 1, 1)) -> None:
+    def __init__(self, shard: int = 60, start_date: datetime = datetime(2010, 1, 1)) -> None:
         self.start_timestamp_block = round(start_date.timestamp() * 10)
         self.shard = shard
+        self.alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        self.alpha_len = len(self.alpha)
         pass
 
-    def number_to_string(self, number: int):
+    def string_to_int(self, string: str) -> int:
+        number = 0
+        for char in string:
+            number = number * self.alpha_len + self.alpha.index(char)
+        return number
+
+    def int_to_string(self, number: int):
         output = ""
         while number:
-            number, digit = divmod(number, alpha_len)
-            output = alpha[digit] + output
-        return output
+            number, digit = divmod(number, self.alpha_len)
+            output += self.alpha[digit]
+        return output[::-1]
 
     def string_from_timestamp(self):
-        number = round(time.time() * 10) - self.start_timestamp_block
-        return self.number_to_string(number=number)
+        number = round(time.time() * 10) - \
+            self.start_timestamp_block
+        return self.int_to_string(number=number)
 
-    def random_shard(self):
-        idx = random.randint(0, 60)
-        return alpha[idx]
+    def get_shard_seq(self, rand_id):
+        return self.string_to_int(rand_id) % self.shard
+
+    def rand_string(self, l):
+        return "".join(random.choices(self.alpha, k=l))
 
     def id(self, l: int = DEFALT_LENGTH):
-        time_id = self.string_from_timestamp()
-        shard_id = self.random_shard()
-        rand_id_l = l - len(time_id) - 1
-        rand_id = "".join(random.choices(alpha, k=rand_id_l))
+        _id, _ = self.id_with_shard(l=l)
+        return _id
 
-        return "{}{}{}".format(time_id, rand_id, shard_id)
+    def id_with_shard(self, l: int = DEFALT_LENGTH):
+        time_id = self.string_from_timestamp()
+        if(l < 7):
+            rand_id_l = 2
+        else:
+            rand_id_l = l - len(time_id)
+
+        rand_id = self.rand_string(l=rand_id_l)
+        shard_seq = self.string_to_int(rand_id) % self.shard
+
+        return "{}{}".format(time_id, rand_id)[-l:], shard_seq
+
+    def decode(self, id: str):
+        time_id = None
+        timestamp = None
+        dt = None
+        if len(id) >= 7:
+            time_id = id[0:6]
+            timestamp = (self.string_to_int(time_id) +
+                         self.start_timestamp_block) / 10
+            dt = datetime.fromtimestamp(timestamp)
+            random_string = id[len(time_id):]
+        else:
+            random_string = id[-2:]
+
+        random_seq = self.string_to_int(random_string)
+        shard_seq = random_seq % self.shard
+
+        return {"shard_seq": shard_seq,
+                "dt": dt,
+                "random_string": random_string,
+                "random_seq": random_seq}
